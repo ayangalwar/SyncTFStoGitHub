7284f99eb164ccd0cb006cf5ef39fc9ca3aa4908
ayangalwar <amit.yangalwar@gmail.com>
2024-06-08 20:40:56 +0530

    Update SyncTFStoGitHub.ps1

--- SyncTFStoGitHub.ps1
+++ SyncTFStoGitHub.ps1
@@ -20,7 +20,7 @@ Write-Host "github Dir : $githubDir"
 $destination = $githubDir+"\"+ $AzureRepoName+".git"
 Write-Host "destination: $destination"
 #Please make sure, you remove https from azure-repo-clone-url
-$sourceURL = "https://$($ADOSourcePAT)"+"@"+"$($ADOCloneURL)"
+$sourceURL = "https://$($SOURCEPAT)"+"@"+"$($ADOCloneURL)"
 write-host "source URL : $sourceURL"
 #Please make sure, you remove https from github-repo-clone-url
 $destURL = "https://" + $($DESTINATIONPAT) +"@"+"$($GitHubCloneURL)"
